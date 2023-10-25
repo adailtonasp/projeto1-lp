d51ed7e7286a9ea5eee239380c7048783c007144
adailtonasp <adailton-hag@hotmail.com>
2023-10-25 19:47:02 -0300

    adicao da funcionalidade 1

---
+++ funcao1.js
@@ -0,0 +1,19 @@
+function verificaDia(ultimaVisita) {
+    let dia = new Date();
+
+    let mesAtual = dia.getMonth();
+
+    let mesUltimaVisita = ultimaVisita.getMonth();
+
+    const diaDoEnvio = 3; //variavel para guarda o dia de segunda
+
+    if (mesAtual === (mesUltimaVisita + 1) % 12) {
+        if (diaDoEnvio === dia.getDay()) {
+            return true;
+        }
+    }
+    console.log("Data invalida");
+    return false;
+}
+
+module.exports = { verificaDia: verificaDia };
